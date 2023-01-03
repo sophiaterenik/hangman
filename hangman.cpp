66395e180436cee23903cd4cbfd9f3ee85b625a2
Sophia Terenik <sophiaterenik@Sophias-MacBook-Air-2.local>
2023-01-03 17:04:03 -0500

    added comments

--- hangman.cpp
+++ hangman.cpp
@@ -5,7 +5,10 @@
 #include <vector>
 using namespace std;
 
-
+/**
+ * @param wrong- number of wrong guesses
+ * prints the hangman depending on how many wrong guesses
+*/
 void print_hangman(int wrong) {
     if (wrong == 0) {
         cout << "+---+" << endl;
@@ -57,10 +60,16 @@ void print_hangman(int wrong) {
         cout << "   ===" << endl;
     }
 }
-
+/**
+ * @param guessedLetters- string of letters guessed
+ * @param randomWord- word that is to be guessed
+ * @return int value of number of right letters guessed
+ * prints the correct letters in the word
+*/
 int printWord(string guessedLetters, string randomWord) {
     int counter = 0;
     int rightLetters = 0;
+    //find guessed letters in random word
     for (int i = 0; i < randomWord.length(); i++){
         if (guessedLetters.find(randomWord[i]) != -1) {
             cout << randomWord[counter];
@@ -79,14 +88,17 @@ int main() {
     cout << "Welcome to hangman" << endl;
     cout << "---------------------------" << endl;
     string dictionary[5] = { "sunflower", "house", "diamond", "memes", "hellow" };
+    //generate random word
     srand(time(NULL));
     string randomWord = dictionary[rand() % 5];
 
+//print empty spaces
     for (unsigned int i = 0; i < randomWord.length(); i++) {
         cout << "_ ";
     }
     cout << endl;
 
+//initialize values
     unsigned int lengthOfWordToGuess = randomWord.length();
     int amountOfTimesWrong = 0;
     int currGuessIndex = 0;
@@ -95,23 +107,30 @@ int main() {
     char letterGuessed;
     string input;
 
+//while game is still going
     while (amountOfTimesWrong != 6 && currLettersRight != lengthOfWordToGuess) {
+        //print letters guessed
         cout << "Letters guessed so far: " << endl;
         for (int i = 0; i < currLettersGuessed.length(); i++){
             cout << currLettersGuessed[i] << " ";
         }
         cout << endl;
+        //read in letter guessed
         cout << "Guess a letter: " << endl;
         getline(cin, input);
         letterGuessed = input[0];
+        //if letter guessed in word
         if (randomWord[currGuessIndex] == letterGuessed) {
+            //print data and update variables
             print_hangman(amountOfTimesWrong);
             currGuessIndex++;
             currLettersGuessed += letterGuessed;
             currLettersRight = printWord(currLettersGuessed, randomWord);
             
         }
+        //if letter guessed not in word
         else {
+            //print data and update variables
             amountOfTimesWrong++;
             currLettersGuessed += letterGuessed;
             print_hangman(amountOfTimesWrong);
